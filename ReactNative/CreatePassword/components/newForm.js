e66fd9ea3c6191f8736e4a831d9ebbec63f23cc3
yanghu <huyang@mail.bistu.edu.cn>
2017-06-08 22:22:53 +0800

    键盘遮挡bug修正

--- ReactNative/CreatePassword/components/newForm.js
+++ ReactNative/CreatePassword/components/newForm.js
@@ -16,8 +16,9 @@ let RealForm = tForm.form.Form;
 let screenHeight = Dimensions.get('window').height;
 import * as createActions from '../actions/actions';
 import Toast from 'react-native-easy-toast'
-
 let isIOS = Platform.OS !== 'android';
+let scrollContainerH = screenHeight - 50 - (isIOS ? 64 : 44);
+
 
 export default class CreateNewForm extends Component {
     constructor(props) {
@@ -26,7 +27,6 @@ export default class CreateNewForm extends Component {
         this.contentHeight = 0;
         console.inputRef = null;//当前编辑的textInput
         this.moveH = 0;//ScrollView滑动的距离
-        this.lastMoveH = 0;//保留上次滑动的距离
         this.needMove = false;//弹出键盘时，inputRef是否需要滑动
     }
     componentWillMount() {
@@ -49,36 +49,32 @@ export default class CreateNewForm extends Component {
         if (!console.inputRef) return;
         this.needMove = false;
         this.refs.form.getComponent(console.inputRef).refs.input.measure((ox, oy, w, h, px, py) => {
-            console.log('py======' + py);
+            // console.log("this.moveH=" + this.moveH, "this.contentHeight=" + this.contentHeight, "height=" + screenHeight);
             let leftHeight = screenHeight - py;//输入框距离底部的距离 = （屏幕的高度 - 当前TextInput的高度）
             //输入框距离底部的距离小于键盘的高度，需要滑动,36是一行输入框的高度
             if (leftHeight < e.startCoordinates.height + 36) {
                 this.needMove = true;
-                // 需要移动的距离
                 let moveHeight = e.startCoordinates.height + 36 - leftHeight;
-                console.log("this.moveH=" + this.moveH, "this.contentHeight=" + this.contentHeight, "height=" + screenHeight);
-
-                // moveH 异常数据处理
-                if (screenHeight > this.contentHeight) {
-                    this.moveH = 0;
-                } else if (this.moveH + py > this.contentHeight) {
-                    this.moveH = this.contentHeight - py;
-                    console.log("===error===");
-                } else if (this.moveH + (py - isIOS ? 64 : 44) < 0) {
-                    this.moveH = 0;
-                }
-                console.log('moveH===' + this.moveH);
-                this.lastMoveH = this.moveH;
-                this.scrollViewTo(this.lastMoveH + moveHeight);
+                this.scrollViewTo(this._dealOffset() + moveHeight);
             }
         });
     }
 
     _keyboardDidHide = () => {
         if (this.needMove) {
-            this.scrollViewTo(this.lastMoveH);
+            this.scrollViewTo(this._dealOffset());
         }
         console.inputRef = null;
+    }
+    _dealOffset() {
+        if (this.moveH <= 0) {
+            this.moveH = 0;
+        } else if (scrollContainerH >= this.contentHeight) {
+            this.moveH = 0;
+        } else if (this.moveH + scrollContainerH > this.contentHeight) {
+            this.moveH = this.contentHeight - scrollContainerH;
+        }
+        return this.moveH;
     }
 
     savePassword() {
@@ -119,6 +115,7 @@ export default class CreateNewForm extends Component {
                     onContentSizeChange={(contentWidth, contentHeight) => {
                         this.contentHeight = parseInt(contentHeight);
                     }}
+                    keyboardShouldPersistTaps={'handled'}
                     onScrollEndDrag={(e) => {
                         this.moveH = e.nativeEvent.contentOffset.y;
                     }} >
@@ -135,26 +132,11 @@ export default class CreateNewForm extends Component {
 const styles = StyleSheet.create({
     containerView: {
         flex: 1,
-        paddingBottom: 40,
+        paddingBottom: 50,
         backgroundColor: '#efeef4',
     },
     container: {
         padding: 15,
         backgroundColor: '#efeef4'
-    },
-    button: {
-        height: 36,
-        backgroundColor: '#48BBEC',
-        borderColor: '#48BBEC',
-        borderWidth: 1,
-        borderRadius: 8,
-        marginBottom: 10,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        fontSize: 18,
-        color: 'white',
-        alignSelf: 'center'
-    },
+    }
 });
