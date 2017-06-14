f8f58e334d8127778176caceea59fa9da309a248
yanghu <huyang@mail.bistu.edu.cn>
2017-06-14 21:52:58 +0800

    获取输入框的值

--- ReactNative/Login/components/login.js
+++ ReactNative/Login/components/login.js
@@ -11,6 +11,8 @@ import ReactNative, {
     Dimensions
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import Toast from 'react-native-easy-toast'
+
 let { height } = Dimensions.get('window');
 
 const loginModules = {
@@ -31,6 +33,18 @@ export default class GestureUnlock extends Component {
             this.props.actions.changeModule(loginModules.login);
         }
     }
+    _submit(loginModule) {
+        let userName = this.refs.userName._lastNativeText;
+        let password = this.refs.password._lastNativeText;
+        if (!userName) {
+            this.refs.toast.show('请输入账号');
+            return;
+        }
+        if (!password) {
+            this.refs.toast.show('请输入密码');
+            return;
+        }
+    }
     render() {
         let { loginModule } = this.props.state;
         let { userNamePlaceholder, passwordPlaceholder } = this.props.state[loginModule];
@@ -41,7 +55,7 @@ export default class GestureUnlock extends Component {
                 } >
                     <Image resizeMode='contain' style={styles.imageSize}
                         source={require('../../Common/images/account.png')} />
-                    <TextInput placeholder={userNamePlaceholder} style={styles.input} clearButtonMode='while-editing' />
+                    <TextInput ref='userName' placeholder={userNamePlaceholder} style={styles.input} clearButtonMode='while-editing' />
                 </TouchableOpacity>
 
                 <TouchableOpacity style={[styles.textView, { marginTop: 10 }]} onPress={
@@ -49,17 +63,19 @@ export default class GestureUnlock extends Component {
                 } >
                     <Image resizeMode='contain' style={styles.imageSize}
                         source={require('../../Common/images/password.png')} />
-                    <TextInput placeholder={passwordPlaceholder} style={styles.input} clearButtonMode='while-editing'
+                    <TextInput ref='password' placeholder={passwordPlaceholder} style={styles.input} clearButtonMode='while-editing'
                         secureTextEntry={true} />
                 </TouchableOpacity>
                 <View style={styles.btnsView} >
-                    <TouchableOpacity style={styles.button} opacity={0.5} onPress={() => { }} >
+                    <TouchableOpacity style={styles.button} opacity={0.5} onPress={() => { this._submit(loginModule) }} >
                         <Text style={styles.buttonText} >{loginModule == loginModules.login ? '登录' : '注册'}</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.registerBtn} opacity={0.5} onPress={() => {this._changeModule(loginModule)}} >
+                    <TouchableOpacity style={styles.registerBtn} opacity={0.5} onPress={() => { this._changeModule(loginModule) }} >
                         <Text style={styles.buttonText} >{loginModule == loginModules.login ? '注册' : '登录'}</Text>
                     </TouchableOpacity>
                 </View>
+
+                <Toast ref="toast" position='center' />
             </View>
         )
     }
